10cc02b80dcc042b41b069dde0befa5a1e25e2fc
rapha-la <97940261+rapha-la@users.noreply.github.com>
2022-03-24 10:23:23 +0100

    Add files via upload

---
+++ CountTaxaPerInterval.R
@@ -0,0 +1,25 @@
+library(dplyr)
+library(tidyverse)
+
+CountTaxaPerInterval = function(df, interval_length){
+  dfcopy = df
+  dfcopy$lower_ends = floor(dfcopy$meantimes/interval_length)*interval_length
+  outdf = cbind(unique(dfcopy$lower_ends),unique(dfcopy$lower_ends))
+  dfcopy = dfcopy %>% select(lower_ends, dataset_ID, meantimes, everything())
+  dfcopy2 = data.frame(dfcopy[4:ncol(dfcopy)])
+  dfcopy2[is.na(dfcopy2)] <- 0
+  dfcopy2[dfcopy2 != 0] <- 1
+  dfcopy = data.frame(dfcopy[1], dfcopy2)
+  for(i in 1:nrow(outdf)){
+    df_restricted = dfcopy[dfcopy$lower_ends==outdf[i,1],]
+    df_restricted = df_restricted[4:ncol(df_restricted)]
+    df_restricted = colSums(df_restricted)
+    outdf[1,2] = length(df_restricted != 0)
+  }
+  outdf = outdf[order(outdf[,1]),]
+  return(outdf)
+}
+
+test2 = CountTaxaPerInterval(coniferous_woodland, 100)
+
+
