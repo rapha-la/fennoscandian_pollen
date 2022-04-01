1b0af7f9840b77a86ee8f400114e1ee90818a361
rapha-la <97940261+rapha-la@users.noreply.github.com>
2022-04-01 14:54:22 +0200

    Update singleLCC_mcp_FF.R

--- singleLCC_mcp_FF.R
+++ singleLCC_mcp_FF.R
@@ -1,4 +1,4 @@
-### mcp on single LCCs (files and functions) ###
+### single LCC mcp (functions and files) ###
 
 #install.packages("plyr")
 #install.packages("dplyr")
@@ -10,8 +10,7 @@ library(funrar)
 library(ecp)
 library(ggplot2)
 
-#LOAD
-LCC <- read.csv("LCC.csv")
+#FILES
 coniferous_woodland <- read.csv("coniferous_woodland.csv")
 deciduous_woodland <- read.csv("deciduous_woodland.csv")
 wet_woodland <- read.csv("wet_woodland.csv")
@@ -19,95 +18,8 @@ wet_meadow <- read.csv("wet_meadow.csv")
 pasture <- read.csv("pasture.csv")
 arable <- read.csv("arable.csv")
 heath <- read.csv("heath.csv")
-coniferous_woodland_all <- read.csv("coniferous_woodland_all.csv")
-deciduous_woodland_all <- read.csv("deciduous_woodland_all.csv")
-wet_woodland_all <- read.csv("wet_woodland_all.csv")
-wet_meadow_all <- read.csv("wet_meadow_all.csv")
-pasture_all <- read.csv("pasture_all.csv")
-heath_all <- read.csv("heath_all.csv")
-arable_all <- read.csv("arable_all.csv")
 
-#mcp on LCC
-mcp_LCC = function(df){
-  mcp_data = df
-  model = list(LCC~age+1, 1~age+1)  # two intercept-only segments?
-  model_null = list(LCC~age+1)
-  fit_mcp = mcp(model, data = mcp_data, par_x = "age")
-  fit_mcp_null = mcp(model_null, data = mcp_data, par_x = "age")
-  summary(fit_mcp)
-  plot(fit_mcp)
-}
-
-mcp2_LCC = function(df){
-  mcp_data = df
-  model = list(LCC~age+1, 1~age+1, 1~age+1)  # three intercept-only segments
-  model_null = list(LCC~age+1)
-  fit_mcp = mcp(model, data = mcp_data, par_x = "age")
-  fit_mcp_null = mcp(model_null, data = mcp_data, par_x = "age")
-  summary(fit_mcp)
-  plot(fit_mcp)
-}
-
-#fit_mcp$loo = loo(fit_mcp)
-#fit_mcp_null$loo = loo(fit_mcp_null)
-#loo::loo_compare(fit_mcp$loo, fit_mcp_null$loo)
-
-
-#ecp_LCC
-ecp_inc = function(groupdf, df){
-  big=groupdf
-  big_sorted = big[order(big$lower_ends),]
-  big_restricted = big_sorted
-  big_restricted_nonas = big_restricted[,colSums(is.na(big_restricted)) < nrow(big_restricted)]
-  big_noids = subset(big_restricted_nonas,select=-c(lower_ends))
-  big_scaled = scale(big_noids,scale = FALSE)
-  ecp_divisive_for_site = e.divisive(big_scaled, k = NULL, min.size = 5)
-  point1 <- big$lower_ends[ecp_divisive_for_site$estimates[2]]
-  point2 <- big$lower_ends[ecp_divisive_for_site$estimates[3]]
-  point3 <- big$lower_ends[ecp_divisive_for_site$estimates[4]]
-  data.frame(df, point1, point2, point3)
-}
-
-#make_interval_sites
-interval_sites = function(dataset, interval, earliest_date, LCCoi){
-  dataset_interval <- dataset
-  dataset_interval$lower_ends = floor(dataset_interval$meantimes/interval)*interval
-  dataset_interval <- dataset_interval %>%
-    group_by(lower_ends) %>%
-    summarise(
-      LCC = sum(LCCoi))
-  dataset_interval <- dataset_interval[-c(103:nrow(dataset_interval)),]
-}
-
-#count sites per interval
-countSitesPerInterval = function(df,interval_length){
-  dfcopy = df
-  dfcopy$lower_ends = floor(dfcopy$meantimes/interval_length)*interval_length
-  outdf = data.frame(lower_ends=unique(dfcopy$lower_ends),NRsites=unique(dfcopy$lower_ends))
-  for(i in 1:nrow(outdf)){
-    df_restricted = dfcopy[dfcopy$lower_ends==outdf[i,1],]
-    outdf[i,2] = length(unique(df_restricted$dataset_ID))
-  }
-  outdf = outdf[order(outdf[,1]),]
-  return(outdf)
-}
-
-#count taxa per interval
-CountTaxaPerInterval = function(df, interval_length){
-  dfcopy = coniferous_woodland
-  dfcopy$lower_ends = floor(dfcopy$meantimes/interval_length)*interval_length
-  dfcopy = dfcopy %>% select(lower_ends, everything())
-  dfcopy =subset(dfcopy, select = -c(dataset_ID, meantimes))
-  dfcopy <- dfcopy %>% group_by(lower_ends) %>% summarise(
-    across(2:ncol(dfcopy)-1,na.rm=TRUE,sum))
-  dfcopy2 = subset(dfcopy, select = -c(lower_ends))
-  dfcopy2[is.na(dfcopy2)] <- 0
-  dfcopy2[dfcopy2 != 0] <- 1
-  dfcopy2 = rowSums(dfcopy2)
-  outdf = data.frame(dfcopy[1], NRtaxa = dfcopy2)
-  return(outdf)
-}
-
+#FUNCTIONS
 #CallSites_N
 CallSites_N = function(df){
   pol15_1 <-  subset(df, dataset_ID == "20")
@@ -148,8 +60,8 @@ CallSites_N = function(df){
                      pol15_18)
 }
 
-#CallSites_S
-CallSites_S = function(df){
+#CallSites_SW
+CallSites_SW = function(df){
   pol1_1 <-  subset(df, dataset_ID == "20050")
   pol1_2 <-  subset(df, dataset_ID == "45704")
   pol1_3 <-  subset(df,dataset_ID == "45707")
@@ -161,10 +73,6 @@ CallSites_S = function(df){
   pol1_9 <-  subset(df,dataset_ID == "45725")
   pol1_10 <-  subset(df,dataset_ID == "45728")
   pol1_11 <-  subset(df,dataset_ID == "45731")
-  pol36_1 <-  subset(df, dataset_ID == "12")
-  pol36_2 <-  subset(df, dataset_ID == "1438")
-  pol36_3 <-  subset(df,dataset_ID == "4403")
-  pol36_4 <-  subset(df,dataset_ID == "45329")
   pol1_all <- rbind(pol1_1,
                     pol1_2,
                     pol1_3,
@@ -175,11 +83,7 @@ CallSites_S = function(df){
                     pol1_8,
                     pol1_9,
                     pol1_10,
-                    pol1_11,
-                    pol36_1,
-                     pol36_2,
-                     pol36_3,
-                     pol36_4)
+                    pol1_11)
 }
 
 #CallSites_MW
@@ -220,6 +124,40 @@ CallSites_MM = function(df){
                      pol47_8)
 }
 
+#CallSites_M
+CallSites_M = function(df){
+  pol47_1 <-  subset(df, dataset_ID == "19906")
+  pol47_2 <-  subset(df, dataset_ID == "19909")
+  pol47_3 <-  subset(df,dataset_ID == "19913")
+  pol47_4 <-  subset(df,dataset_ID == "20018")
+  pol47_5 <-  subset(df, dataset_ID == "21790")
+  pol47_6 <-  subset(df, dataset_ID == "45351")
+  pol47_7 <-  subset(df,dataset_ID == "45698")
+  pol47_8 <-  subset(df,dataset_ID == "45701")
+  pol2_1 <-  subset(df, dataset_ID == "977")
+  pol2_2 <-  subset(df, dataset_ID == "20042")
+  pol2_3 <-  subset(df,dataset_ID == "20046")
+  pol2_4 <-  subset(df,dataset_ID == "45331")
+  pol2_5 <-  subset(df,dataset_ID == "45345")
+  pol2_6 <-  subset(df,dataset_ID == "45347")
+  pol2_7 <-  subset(df,dataset_ID == "45349")
+  pol47_all <- rbind(pol47_1,
+                     pol47_2,
+                     pol47_3,
+                     pol47_4,
+                     pol47_5,
+                     pol47_6,
+                     pol47_7,
+                     pol47_8,
+                     pol2_1,
+                     pol2_2,
+                     pol2_3,
+                     pol2_4,
+                     pol2_5,
+                     pol2_6,
+                     pol2_7)
+}
+
 #CallSites_SE
 CallSites_SE = function(df){
   pol911_1 <-  subset(df, dataset_ID == "4092")
@@ -248,15 +186,98 @@ CallSites_SE = function(df){
                       pol911_12)
 }
 
-#ecp_sums
-ecp_sums = function(groupdf, df){
-  big=groupdf
-  big_sorted = big[order(big$age),]
-  ecp_divisive_for_site = e.divisive(big_sorted, k = NULL, min.size = 5)
-  point1 <- big$age[ecp_divisive_for_site$estimates[2]]
-  point2 <- big$age[ecp_divisive_for_site$estimates[3]]
-  point3 <- big$age[ecp_divisive_for_site$estimates[4]]
-  data.frame(df, point1, point2, point3)
+#CallSites_SM
+CallSites_SM = function(df){
+  pol36_1 <-  subset(df, dataset_ID == "12")
+  pol36_2 <-  subset(df, dataset_ID == "1438")
+  pol36_3 <-  subset(df,dataset_ID == "4403")
+  pol36_4 <-  subset(df,dataset_ID == "45329")
+  pol36_all <- rbind(pol36_1,
+                     pol36_2,
+                     pol36_3,
+                     pol36_4)
+}
+
+#CallSites_S
+CallSites_S = function(df){
+  pol911_1 <-  subset(df, dataset_ID == "4092")
+  pol911_2 <-  subset(df, dataset_ID == "4133")
+  pol911_3 <-  subset(df,dataset_ID == "4156")
+  pol911_4 <-  subset(df,dataset_ID == "4168")
+  pol911_5 <-  subset(df, dataset_ID == "4259")
+  pol911_6 <-  subset(df, dataset_ID == "4393")
+  pol911_7 <-  subset(df,dataset_ID == "4420")
+  pol911_8 <-  subset(df,dataset_ID == "4472")
+  pol911_9 <-  subset(df, dataset_ID == "4539")
+  pol911_10 <-  subset(df, dataset_ID == "4543")
+  pol911_11 <-  subset(df,dataset_ID == "4017")
+  pol911_12 <-  subset(df,dataset_ID == "3928")
+  pol36_1 <-  subset(df, dataset_ID == "12")
+  pol36_2 <-  subset(df, dataset_ID == "1438")
+  pol36_3 <-  subset(df,dataset_ID == "4403")
+  pol36_4 <-  subset(df,dataset_ID == "45329")
+  pol911_all <- rbind(pol911_1,
+                      pol911_2,
+                      pol911_3,
+                      pol911_4,
+                      pol911_5,
+                      pol911_6,
+                      pol911_7,
+                      pol911_8,
+                      pol911_9,
+                      pol911_10,
+                      pol911_11,
+                      pol911_12,
+                      pol36_1,
+                      pol36_2,
+                      pol36_3,
+                      pol36_4)
+}
+
+#make_interval_pol
+make_interval_pol = function(dataset, interval, earliest_date){
+  dataset_interval <- dataset
+  dataset_interval$lower_ends = floor(dataset_interval$meantimes/interval)*interval
+  dataset_interval <- subset(dataset_interval, select = -c(dataset_ID))
+  dataset_interval <- dataset_interval %>%
+    group_by(lower_ends) %>%
+    summarise(
+      meantimes = mean(meantimes),
+      across(2:ncol(dataset_interval)-1,na.rm=TRUE,sum))
+  dataset_interval <- dataset_interval[-c(103:nrow(dataset_interval)),]
+}
+
+
+#mcp on LCC
+mcp_LCC = function(df){
+  mcp_data = df
+  model = list(LCC~age+1, 1~age+1)  # two intercept-only segments?
+  model_null = list(LCC~age+1)
+  fit_mcp = mcp(model, data = mcp_data, par_x = "age")
+  fit_mcp_null = mcp(model_null, data = mcp_data, par_x = "age")
+  summary(fit_mcp)
+  plot(fit_mcp)
+}
+
+mcp2_LCC = function(df){
+  mcp_data = df
+  model = list(LCC~age+1, 1~age+1, 1~age+1)  # three intercept-only segments
+  model_null = list(LCC~age+1)
+  fit_mcp = mcp(model, data = mcp_data, par_x = "age")
+  fit_mcp_null = mcp(model_null, data = mcp_data, par_x = "age")
+  summary(fit_mcp)
+  plot(fit_mcp)
+}
+
+#fit_mcp$loo = loo(fit_mcp)
+#fit_mcp_null$loo = loo(fit_mcp_null)
+#loo::loo_compare(fit_mcp$loo, fit_mcp_null$loo)
+
+#make increments
+makeIncrements = function(df){
+  df_inc <- df[2:(nrow(df)),2:ncol(df)]-df[1:(nrow(df)-1),2:ncol(df)]
+  df_inc$lower_ends = df$lower_ends[2:nrow(df)]
+  return(df_inc)
 }
 
 #make sqrt
@@ -269,4 +290,65 @@ sqrt_sums = function(df){
 #make increments
 makeIncrements_singleLCC = function(df){
   df[2:(nrow(df)),2:ncol(df)]-df[1:(nrow(df)-1),2:ncol(df)]
+}
+
+#ecp_sums
+ecp_sums = function(groupdf, df){
+  big=groupdf
+  big_sorted = big[order(big$age),]
+  ecp_divisive_for_site = e.divisive(big_sorted, k = NULL, min.size = 5)
+  point1 <- big$age[ecp_divisive_for_site$estimates[2]]
+  point2 <- big$age[ecp_divisive_for_site$estimates[3]]
+  point3 <- big$age[ecp_divisive_for_site$estimates[4]]
+  data.frame(df, point1, point2, point3)
+}
+
+#ecp_inc
+#ecp_inc = function(groupdf, df){
+#  big=groupdf
+#  big_sorted = big[order(big$lower_ends),]
+#  big_restricted = big_sorted
+#  big_restricted_nonas = big_restricted[,colSums(is.na(big_restricted)) < nrow(big_restricted)]
+#  big_noids = subset(big_restricted_nonas,select=-c(lower_ends))
+#  big_scaled = scale(big_noids,scale = FALSE)
+#  ecp_divisive_for_site = e.divisive(big_scaled, k = NULL, min.size = 5)
+#  point1 <- big$lower_ends[ecp_divisive_for_site$estimates[2]]
+#  point2 <- big$lower_ends[ecp_divisive_for_site$estimates[3]]
+#  point3 <- big$lower_ends[ecp_divisive_for_site$estimates[4]]
+#  data.frame(df, point1, point2, point3)
+#}
+
+#plot_LCC
+plot_LCCecp = function(LCC_df, ecp_df, titleinquotes){
+  ggplot(LCC_df, aes(x = age)) + 
+    scale_colour_manual(values=c(col1="darkgreen",col2="green2", col3="darkslategray3", col4="violet", col5="tan3", col6="red2", col7="blue", changepoint="black") ,
+                        labels=c("coniferous_woodland","deciduous_woodland","wet_woodland","pasture/meadow","wet_meadow","arable","heath", "changepoint")) +
+    geom_line(aes(y = conN,colour="col1")) + 
+    geom_line(aes(y = decN,colour="col2")) + 
+    geom_line(aes(y = wetwN,colour="col3")) +
+    geom_line(aes(y = wetmN,colour="col4")) + 
+    geom_line(aes(y = pasN,colour="col5")) + 
+    geom_line(aes(y = araN,colour="col6")) +
+    geom_line(aes(y = heaN,colour="col7")) + 
+    geom_vline(aes(xintercept = ecp_df$point1, colour = "changepoint")) +
+    geom_vline(aes(xintercept = ecp_df$point2, colour = "changepoint")) +
+    geom_vline(aes(xintercept = ecp_df$point3, colour = "changepoint")) +
+    ggtitle(titleinquotes) +
+    labs(x = "Years BP",y = "counts",colour = "Legend")
+}
+
+#count taxa per interval
+CountTaxaPerInterval = function(df, interval_length){
+  dfcopy = coniferous_woodland
+  dfcopy$lower_ends = floor(dfcopy$meantimes/interval_length)*interval_length
+  dfcopy = dfcopy %>% select(lower_ends, everything())
+  dfcopy =subset(dfcopy, select = -c(dataset_ID, meantimes))
+  dfcopy <- dfcopy %>% group_by(lower_ends) %>% summarise(
+    across(2:ncol(dfcopy)-1,na.rm=TRUE,sum))
+  dfcopy2 = subset(dfcopy, select = -c(lower_ends))
+  dfcopy2[is.na(dfcopy2)] <- 0
+  dfcopy2[dfcopy2 != 0] <- 1
+  dfcopy2 = rowSums(dfcopy2)
+  outdf = data.frame(dfcopy[1], NRtaxa = dfcopy2)
+  return(outdf)
 }
