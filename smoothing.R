ab3fc7ddbf0ad91635ce20855dda62f05c35c345
rapha-la <97940261+rapha-la@users.noreply.github.com>
2022-07-22 14:25:38 +0200

    Update smoothing.R

--- smoothing.R
+++ smoothing.R
@@ -11,6 +11,7 @@ library(ggplot2)
 library(tidyverse)      # data manipulation and visualization
 library(lubridate)      # easily work with dates and times
 library(fpp2)           # working with time series data
+library(smoothing)
 library(zoo) 
 library(TTR)
 
