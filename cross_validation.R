042fe63e7d5c87956efa00fb2b2b034fc8f45153
Jakub Truszkowski <jakub.truszkowski@gmail.com>
2022-08-24 17:27:02 +0200

    seems to work on this one data set

--- cross_validation.R
+++ cross_validation.R
@@ -2,16 +2,29 @@
 
 library(modelr)
 library(glm2)
+library(timetk)
+
+nfolds = 2
+maxlag = 7
 
 varnames <- c(deparse(substitute(LCCadapt)), deparse(substitute(SPD)), deparse(substitute(clim)))
-lagnames <- paste0(varnames, "_lag", rep(1:7, each = 3))
+lagnames <- paste0(varnames, "_lag", rep(1:maxlag, each = 3))
 # Then we created the lagged variables / data for models
-VAR_data <- embed(as.matrix(cbind(conNs, SPD.N, clim.N)), 7 + 1)
+VAR_data <- embed(as.matrix(cbind(conNs, SPD.N, clim.N)), maxlag + 1)
 colnames(VAR_data) <- c(varnames, lagnames)
 VAR_data <- as.data.frame(VAR_data[, -c(2,3)])
 
-dataconN_cv  <- crossv_kfold(VAR_data, k = 5)
+dataconN_cv  <- crossv_kfold(VAR_data, k = nfolds) # just an old structure that we'll fill out differently
 
+chunksize = ceiling(nrow(VAR_data)/(nfolds+1))
+BP = -yearsBP[(maxlag + 1):length(yearsBP)] # so that time goes up with time
+VAR_data_ext = cbind(BP,VAR_data)
+tscv <- time_series_cv(data = VAR_data_ext, date_var = BP, initial = chunksize, skip = chunksize - 1, assess = chunksize - 1, cumulative = TRUE)
+
+for(i in 1:nfolds){
+  dataconN_cv$train[[i]]$idx <- tscv$splits[[i]]$in_id
+  dataconN_cv$test[[i]]$idx <- tscv$splits[[i]]$out_id
+}
 # the full model with LCC, SPD and clim
 formula7 <- paste(colnames(VAR_data)[-1], collapse = " + ")
 formula7 <- formula(paste0(varnames[1], " ~ ", formula7))
@@ -40,13 +53,13 @@ formula1 <- select(VAR_data,-contains(c("7", "6", "5", "4", "3", "2")))
 formula1 <- paste(colnames(formula1)[-1], collapse = " + ")
 formula1 <- formula(paste0(varnames[1], " ~ ", formula1))
 
-model7  <- map(dataconN_cv$train, ~glm2(formula7, data = VAR_data))
-model6  <- map(dataconN_cv$train, ~glm2(formula6, data = VAR_data))
-model5  <- map(dataconN_cv$train, ~glm2(formula5, data = VAR_data))
-model4  <- map(dataconN_cv$train, ~glm2(formula4, data = VAR_data))
-model3  <- map(dataconN_cv$train, ~glm2(formula3, data = VAR_data))
-model2  <- map(dataconN_cv$train, ~glm2(formula2, data = VAR_data))
-model1  <- map(dataconN_cv$train, ~glm2(formula1, data = VAR_data))
+model7  <- map(dataconN_cv$train, ~glm2(formula7, data = .))
+model6  <- map(dataconN_cv$train, ~glm2(formula6, data = .))
+model5  <- map(dataconN_cv$train, ~glm2(formula5, data = .))
+model4  <- map(dataconN_cv$train, ~glm2(formula4, data = .))
+model3  <- map(dataconN_cv$train, ~glm2(formula3, data = .))
+model2  <- map(dataconN_cv$train, ~glm2(formula2, data = .))
+model1  <- map(dataconN_cv$train, ~glm2(formula1, data = .))
 
 get_pred  <- function(model, test_data){
   data  <- as.data.frame(test_data)
@@ -89,3 +102,4 @@ mean(MSE4$MSE)
 mean(MSE5$MSE)
 mean(MSE6$MSE)
 mean(MSE7$MSE)
+
